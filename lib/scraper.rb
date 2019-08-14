989e72cec1a03ffa729c7c9452db7ac01d944966
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-14 19:36:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,11 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc.css(".tout_label.heading.heading--level-4")
+
+html = open("https://flatironschool.com/")
+Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(html)
 
 
